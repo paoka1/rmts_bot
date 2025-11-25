c66985e641dcbf73c5a589e78b25a613d8056dae
paoka1 <shauio@163.com>
2025-11-25 12:22:44 +0800

    优化戳一戳事件响应，随机选择戳一戳消息文本

--- rmts/plugins/chat/__init__.py
+++ rmts/plugins/chat/__init__.py
@@ -1,3 +1,5 @@
+import random
+
 from nonebot import get_driver
 from nonebot import on_message, on_notice
 from nonebot.rule import to_me, Rule
@@ -26,13 +28,20 @@ async def rmts_chat(event: MessageEvent):
         MessageSegment.reply(message_id) + f"{reply}"
     )
 
+poke_msgs = ["博士（TA的名字是：{}）戳了戳你",
+             "博士（TA的名字是：{}）轻轻地戳了戳你",
+             "博士（TA的名字是：{}）拍了拍你",
+             "博士（TA的名字是：{}）向你打招呼",
+             "博士（TA的名字是：{}）摸了摸你",
+             "你看见了博士（TA的名字是：{}）"]
+
 # 使用自定义 rule 创建事件响应器
 poke_handler = on_notice(rule=Rule(is_poke_me), priority=3, block=True)
 
 @poke_handler.handle()
 async def handle_poke(bot: Bot, event: PokeNotifyEvent):
     nickname = await get_nickname(bot, event.group_id, event.user_id)
-    text = f"博士（TA的名字是：{nickname}）戳了戳你"
+    text = random.choice(poke_msgs).format(nickname)
     reply = rmts.chat(text)
     await poke_handler.send(MessageSegment.at(event.user_id) + f" {reply}")
 
