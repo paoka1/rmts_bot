829378fe4f8abfa1574ff35c3af2f2e9dd7de6ba
paoka1 <shauio@163.com>
2025-11-28 20:22:16 +0800

    修复split_groups函数，确保输入为字符串类型以正确分割群列表

--- rmts/utils/config.py
+++ rmts/utils/config.py
@@ -1,5 +1,9 @@
-def split_groups(groups_str: str) -> list[str]:
-    groups = groups_str.split(",")
+def split_groups(groups_str) -> list[str]:
+    """
+    分割群列表
+    """
+    groups = str(groups_str).split(",")
     if groups == [""]:
         return []
     return groups
+
