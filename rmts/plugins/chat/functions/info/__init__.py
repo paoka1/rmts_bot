7a87adf1eabfeb1e04633b812f5c7db04e463035
paoka1 <shauio@163.com>
2025-12-07 14:45:36 +0800

    优化获取当前时间的函数描述和返回格式，使用更清晰的时间字符串表示

--- rmts/plugins/chat/functions/info/__init__.py
+++ rmts/plugins/chat/functions/info/__init__.py
@@ -2,13 +2,13 @@
 和获取信息有关的函数调用功能
 """
 
+from datetime import datetime
 from rmts.plugins.chat.function_calling import FunctionDescription, function_container
 
 # 获取当前时间
-func_desc_time = FunctionDescription(name="get_current_time", description="获取当前系统时间")
-func_desc_time.add_return(name="current_time", description="当前系统时间的字符串表示")
+func_desc_time = FunctionDescription(name="get_current_time", description="获取当前时间")
+func_desc_time.add_return(name="current_time", description="当前时间的字符串表示")
 
 @function_container.function_calling(func_desc_time)
 def get_current_time() -> str:
-    from datetime import datetime
-    return datetime.now().isoformat()
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
