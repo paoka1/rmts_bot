712404a054c95b0907888026448cbcbe0c05988d
paoka1 <shauio@163.com>
2025-12-08 10:25:32 +0800

    重命名戳一戳功能为“戳一戳指定博士”，更新参数描述和返回信息

--- rmts/plugins/chat/functions/action/__init__.py
+++ rmts/plugins/chat/functions/action/__init__.py
@@ -6,17 +6,17 @@ from nonebot import get_bot
 from rmts.plugins.chat.function_calling import FunctionDescription, function_container
 
 # 戳一戳
-func_desc_poke = FunctionDescription(name="poke_user", description="戳一戳指定用户")
-func_desc_poke.add_parameter(name="id", description="用户的唯一标识符", param_type="string")
+func_desc_poke = FunctionDescription(name="poke_doctor", description="戳一戳指定博士")
+func_desc_poke.add_parameter(name="id", description="博士的唯一标识符", param_type="string")
 func_desc_poke.add_injection_parameter(name="group_id", description="群组的唯一标识符")
 func_desc_poke.add_return(name="result", description="操作结果")
 
 @function_container.function_calling(func_desc_poke)
-async def poke_user(id: int, group_id: int) -> str:
+async def poke_doctor(id: int, group_id: int) -> str:
     bot = get_bot()
     await bot.call_api(
         "send_poke",
         user_id=id,
         group_id=group_id
     )
-    return f"你戳了戳ID为{id}的用户"
+    return f"你戳了戳ID为{id}的博士"
