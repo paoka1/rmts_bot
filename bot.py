d811839737b2ff30d7e13b5802685b38474e7e6f
paoka1 <shauio@163.com>
2026-02-12 15:23:34 +0800

    移除未使用的控制台适配器导入

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
 import nonebot
 
 from nonebot.adapters.onebot.v11 import Adapter as ONEBOT_V11Adapter
-# from nonebot.adapters.console import Adapter as CONSOLEAdapter
 
 nonebot.init()
 
 driver = nonebot.get_driver()
 driver.register_adapter(ONEBOT_V11Adapter)
-# driver.register_adapter(CONSOLEAdapter)
 
 # nonebot.load_builtin_plugins('echo')
 nonebot.load_from_toml("pyproject.toml")
